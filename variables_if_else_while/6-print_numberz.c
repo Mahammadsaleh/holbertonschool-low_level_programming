69da182d20a3f5e42b4312ae6db4164d38da82ff
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-16 06:30:30 -0700

    added

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function generates a random number and determines if it's positive,
+ * negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
+
