cb62a5ba47f8816c508c2f21efe9a167682c6d1c
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-16 05:16:34 -0700

    added

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * This function generates a random number and determines if it's positive,
+ * negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}			}
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
