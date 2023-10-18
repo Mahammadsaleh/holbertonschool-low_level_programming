cee0e503e2423f0a9d5e6dc6ac5c73ec63e224b5
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-18 10:54:47 -0700

    added -10th task

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - additon
+ * @num1: number one
+ * @num2: number two
+ * Return: num1+num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
