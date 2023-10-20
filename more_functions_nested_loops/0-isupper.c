ca0b7b9ca1f1216fdf356a584ba610a718ca5721
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-20 13:06:07 -0700

    added -5h task

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - returns 1 if char is lowercase
+ *
+ * @c: character
+ *
+ * Return: if Lower 1 else 0
+ **/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{	
+	return (1);
+	}
+
+	return (0);
+}
