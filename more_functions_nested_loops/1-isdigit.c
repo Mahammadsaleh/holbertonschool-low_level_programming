4adcd25f16d86025bbf0230d1207983cfd7bf005
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-20 15:48:32 -0700

    added -2rd task

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - returns 1 if digit
+ *
+ * @c: character 
+ *
+ * Return: 1 if digit
+ **/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
