dd985b249adb6a1efc81737fcc90316aae70cc3f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-23 15:19:49 -0700

    added

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints string
+ * @str: character
+ *
+ * Return: void
+ */
+
+void print_rev(char *str)
+{
+		char last;
+		int i = 0;
+
+		while (last != '\0')
+		{
+			last = *(str + i);
+			i++;
+		}
+
+		while (i != -1)
+		{
+			last = *(str + i);
+			_putchar(last);
+			i--;
+		}
+		_putchar('\n');
+}
