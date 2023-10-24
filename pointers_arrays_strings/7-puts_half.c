ff3481233128ece9f9b6009f3a34426d983685db
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-24 00:20:12 -0700

    added -8th task

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - print half.
+ *
+ * @s: The string to be reversed.
+ */
+void puts_half(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	if ((i - 1) % 2 == 1)
+	{
+		i = (i) / 2;
+		while (s[i] != '\0')
+		{
+			_putchar(s[i]);
+			i++;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (s[i] != '\0')
+		{
+			_putchar(s[i]);
+			i++;
+		}
+	}
+
+	_putchar('\n');
+}
+
