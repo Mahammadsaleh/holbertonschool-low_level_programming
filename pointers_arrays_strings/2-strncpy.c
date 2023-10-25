bb9691bdf1b599b2a59af36ad25fac789368f01d
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-25 06:13:20 -0700

    added

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -5,15 +5,23 @@
  *@dest: string 1
  *
  *@src: string 2
+ *@n: number
  *
  * Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
+
+	if (i < n)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
