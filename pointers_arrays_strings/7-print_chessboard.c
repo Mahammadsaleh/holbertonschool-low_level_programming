7eb068664888deeb34ba27c8151f4bd62088d0ec
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-28 08:59:11 -0700

    added

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -9,6 +9,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
@@ -16,7 +17,9 @@ void print_chessboard(char (*a)[8])
 			_putchar(a[i][j]);
 		}
 		if (n != 7)
+		{
 			_putchar('\n');
+		}
 	}
 	_putchar('\n');
 }
