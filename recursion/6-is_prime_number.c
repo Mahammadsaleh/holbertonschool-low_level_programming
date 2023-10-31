dd23f6101b887a11ba36b0cfd04ba8e9e054244e
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-31 05:03:06 -0700

    added

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,45 @@
 /**
+ * checker - checker
+ * @n: number
+ * @m: devider
+ *
+ * Return: int
+ */
+
+int checker(int n, int m)
+{
+	if (m == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % m == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (checker(n, m - 1));
+		}
+	}
+}
+
+/**
  * is_prime_number- is Prime
  * @n: number
  *
  * Return: int
  */
-
 int is_prime_number(int n)
 {
-	static int i = 2; 
-
-	if (n == 0 || n == 1)
-	{
-		return (0);
-	} 
-
-	if (n == i)
-		return (1);
-
-	if (n % i == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
- 
-	i++;
+	int check;
 
-	return is_prime_number(n);
+	check = checker(n, n / 2);
+
+	return (check);
 }
