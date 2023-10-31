0018d347da1ac60ef8866d8690a6f9de65d2f992
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-10-31 00:53:41 -0700

    added

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/**
+ * factorial- find fact
+ * @n: number
+ *
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+	{
+		return (1);
+	}
+	if (n > 0)
+	{
+		n = n * factorial(n - 1);
+	}
+	else
+		return (-1);
+
+	return (n);
+}
