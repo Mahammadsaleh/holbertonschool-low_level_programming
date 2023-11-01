9d6c3a4f79461a876a931430cd1149d87f88e0da
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-01 03:34:08 -0700

    added

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+        (void)argv;
+        printf("%d\n", argc);
+
+        return (0);
+}
