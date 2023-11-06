ee3965a631041d82a53063c8c9ca7712f449999e
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-06 13:00:51 -0800

    added

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,45 +1,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 /**
- * *str_concat -  concatenates two strings.
- * @s1: string1
- * @s2: string2
+ * concat_strings - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
  *
- * Return: char
+ * Return: A newly allocated concatenated string, or NULL on failure.
+ */
+char *concat_strings(char *s1, char *s2)
+{
+	if (s1 == NULL)
+	{
+		return (strdup(s2));
+	}
+	else if (s2 == NULL)
+	{
+		return (strdup(s1));
+	}
+	else
+	{
+		char *conc = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+
+		if (conc == NULL)
+		{
+			return (NULL);
+		}
+
+		strcpy(conc, s1);
+		strcat(conc, s2);
+		return (conc);
+	}
+}
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A newly allocated concatenated string, or NULL on failure.
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int size;
-	char *conc;
-	int i;
-	int j;
-
 	if (s1 == NULL && s2 == NULL)
 	{
 		return (NULL);
 	}
 
-	size = strlen(s1) + strlen(s2) + 1;
-	conc = (char *)malloc((int)size * sizeof(char));
-
-	if (conc == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < (int)strlen(s1); i++)
-	{
-		conc[i] = s1[i];
-	}
-
-	for (j = 0; j < (int)size; j++)
-	{
-		conc[i] = s2[j];
-		i++;
-	}
-
-	conc[i] = '\0';
-
-	return (conc);
+	return (concat_strings(s1, s2));
 }
+
