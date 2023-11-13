68488e3fd1211aae8cf890f793c23d60da4cb167
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-12 23:57:02 -0800

    added

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 	unsigned int j;
 
+	if (s2 == NULL)
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			concat[i] = s1[i];
+		}
+		concat[i] = '\0';
+		return (concat);
+	}
+
 	if (concat == NULL)
 	{
 		return (NULL);
