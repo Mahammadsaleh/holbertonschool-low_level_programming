0a91bc7a2fe1c0c2a026368d3b2e57f9db9ce10f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-13 00:05:36 -0800

    added

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -6,24 +6,22 @@
  * @s1: string 1
  * @s2: string 2
  * @n: size
+ *
+ * Return: char
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat = malloc(strlen(s1) + n + 1);
+	char *concat;
 	unsigned int i;
 	unsigned int j;
 
+	if (s1 == NULL)
+		s1 = "";
 	if (s2 == NULL)
-	{
-		for (i = 0; i < strlen(s1); i++)
-		{
-			concat[i] = s1[i];
-		}
-		concat[i] = '\0';
-		return (concat);
-	}
+		s2 = "";
 
+	concat = malloc(strlen(s1) + n + 1);
 	if (concat == NULL)
 	{
 		return (NULL);
