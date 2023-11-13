99f17baed46befc92bdd119bdbad41982373b481
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-12 23:49:54 -0800

    added

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * *string_nconcat - conacatinate the strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat = malloc(strlen(s1) + n + 1);
+	unsigned int i;
+	unsigned int j;
+
+	if (concat == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < strlen(s1); i++)
+	{
+		concat[i] = s1[i];
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+
+	concat[i] = '\0';
+	return (concat);
+
+}
