9eea01f6b175df0951c31808d103051d7bca1b58
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-23 04:10:16 -0800

    added

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+/**
+ * print_name - print name
+ * @name: str
+ * @f: function
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	f(name);
+}
