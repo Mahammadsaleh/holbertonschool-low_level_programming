8550753136262b4489030b13b726ac0fc62ef20a
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-22 23:54:21 -0800

    added

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,7 +13,6 @@ typedef struct dog
 	char *owner;
 } dog_t;
 void free_dog(dog_t *d);
-void dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
