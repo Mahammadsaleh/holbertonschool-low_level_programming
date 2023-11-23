effa50fcdd528860c1961350b739af6fd30645ba
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-22 23:45:14 -0800

    added

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,8 +12,9 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-		printf("Age: %.6f\n", (d->age != NULL) ? d->age : 0);
+		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 	}
 }
+
 
