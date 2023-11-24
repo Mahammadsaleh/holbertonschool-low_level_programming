4e1b25f5577e777bd2e7b31af15f0ac87cd70ccb
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-11-24 14:30:24 -0800

    added

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,32 +1,72 @@
 #include "3-calc.h"
-
-/* Functions for arithmetic operations */
-
-int op_add(int a, int b) {
-    return a + b;
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
 }
 
-int op_sub(int a, int b) {
-    return a - b;
+/**
+ * op_sub - Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The difference between a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
 }
 
-int op_mul(int a, int b) {
-    return a * b;
+/**
+ * op_mul - Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
 }
 
-int op_div(int a, int b) {
-    if (b == 0) {
-        printf("Error\n");
-        exit(100);
-    }
-    return a / b;
+/**
+ * op_div - Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The result of dividing a by b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
-int op_mod(int a, int b) {
-    if (b == 0) {
-        printf("Error\n");
-        exit(100);
-    }
-    return a % b;
+/**
+ * op_mod - Calculates the modulo of two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
-
