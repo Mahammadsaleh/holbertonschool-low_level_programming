4d320b20a4a593be55db4423c7f139402939365e
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-05 01:19:28 -0800

    0-print_list.c file added

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	unsigned int counter = 0;
+
+	while ( h != NULL)
+	{
+		counter++;
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}
+	return (counter);
+}
