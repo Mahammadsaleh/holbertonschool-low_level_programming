b9d7978318e2205e0c3f9415f50b1571f944cc34
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-06 22:13:22 -0800

    fixed

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -14,44 +14,25 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *current = *head;
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
-		*head = (list_t *)malloc(sizeof(list_t));
-		if (*head == NULL)
-		{
-			free(*head);
-			return (NULL);
-		}
-		(*head)->str = strdup(str);
-		if ((*head)->str == NULL)
-		{
-			free(new_node);
-                	return (NULL);
-		}
-		(*head)->len = strlen(str);
-		(*head)->next = NULL;
-		return (*head);
+		*head = new_node;
+		return (new_node);
 	}
 
 	while (current->next != NULL)
 	{
 		current = current->next;
 	}
-
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = NULL;
 
 	current->next = new_node;
 	return (*head);
