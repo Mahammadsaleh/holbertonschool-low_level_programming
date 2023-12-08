55445f0d85b592f3014a79a8fcfb18a22254d6a0
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-08 15:18:56 -0800

    pushed

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - add first item double list
+ * @head: list_t type node
+ * @index: index
+ *
+ * Return: void
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *cur = *head;
+	dlistint_t *tmp;
+	unsigned int counter = 0;
+
+	if (index == 0)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		while (cur->next != NULL && index - 1 != counter)
+		{
+			cur = cur->next;
+			counter++;
+		}
+		if (index - 1 > counter)
+		{
+			return (-1);
+		}
+		else
+		{
+			tmp = cur->next;
+			cur->next = cur->next->next;
+			free(tmp);
+			if (cur->next != NULL)
+				cur->next->prev = cur;
+
+		}
+	}
+	return (1);
+}
