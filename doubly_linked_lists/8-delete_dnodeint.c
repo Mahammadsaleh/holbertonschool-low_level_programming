368f9738f1f2168b20791283e09003a1bb6b8aac
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-08 15:44:50 -0800

    pushed

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,18 +15,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int counter = 0;
 
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
 	if (index == 0)
 	{
-		if ( *head != NULL)
+		if (*head != NULL)
 		{
 			tmp = *head;
 			*head = (*head)->next;
+			if (*head != NULL)
+			{
+				(*head)->prev = NULL;
+			}
 			free(tmp);
 			return (1);
-		}
-		else
-		{
-			return (-1);
 		}
 	}
 	else
