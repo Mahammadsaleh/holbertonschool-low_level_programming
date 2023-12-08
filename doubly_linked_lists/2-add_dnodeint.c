1b0fcdd0e830209f778ead173c2d66749daae16f
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-08 00:08:12 -0800

    linked list add func added

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		*head = new_node;
