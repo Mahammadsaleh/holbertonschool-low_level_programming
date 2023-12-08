87da5eb4e25cd654b9c2993eb9d770b85af235d5
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-08 00:06:34 -0800

    linked list add func added

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add first item double list
+ * @head: list_t type node
+ * @n: num
+ *
+ * Return: void
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->n = n;
+		new_node->prev = (*head)->prev;
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	return (*head);
+
+}
