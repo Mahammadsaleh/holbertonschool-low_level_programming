f6d06b9d33d55e4a08e9e1bf076b25c35632f04b
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-08 04:48:20 -0800

    find index func was added

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - add first item double list
+ * @head: list_t type node
+ * @index: num
+ *
+ * Return: void
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *cur = head;
+	unsigned int j = 0;
+
+	while (index != j)
+	{
+		cur = cur->next;
+		j++;
+	}
+
+	return (cur);
+
+}
