9129fd915ddab1dbaa7419684a105be23b3cf740
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-09 05:18:27 -0800

    added

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -8,6 +8,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
+
 	if (b == NULL)
 		return (0);
 
@@ -17,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		{
 			return (0);
 		}
-		
+
 		result = (result << 1) + (*b - '0');
 		b++;
 	}
