12d17944685e4f129b8573cd0884068b477792fc
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-10 05:31:29 -0800

    added

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * clear_bit - clear the value of a bit at a given index
+ * @n: number to check
+ * @index: index to check
+ *
+ * Return: int ptr
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 0x01;
+	unsigned long int temp = *n;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
+	mask <<= index;
+	temp &= ~mask;
+
+	if (temp == *n)
+	{
+		return (0);
+	}
+	else
+	{
+		*n = temp;
+		return (1);
+	}
+}
