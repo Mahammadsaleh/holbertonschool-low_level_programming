f1193bd4f0c2068e454a93394ae34deb4420a8c2
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-12 13:21:32 -0800

    fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = creat(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = creat(filename, O_RDWR | O_CREAT);
 	w = write(fd, text_content, strlen(text_content));
 	if (fd == -1 || w == -1)
 	{
