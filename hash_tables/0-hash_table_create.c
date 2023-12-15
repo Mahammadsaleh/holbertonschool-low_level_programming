41301a28b5d5e6955a6ff9906f841fe11e4d1a55
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2023-12-14 16:11:04 -0800

    test

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,17 +10,12 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t *array = malloc(sizeof(hash_node_t)*size);
 	hash_table_t *hash_t;
 
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-
-	hash_t = malloc(sizeof(array) + size);
+	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
 		return (NULL);
+	hash_t->size = size;
 
 	return (hash_t);
 }
