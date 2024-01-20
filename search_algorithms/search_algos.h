69070dd3bc6e012fb4bd220910a14240df62bd48
Mahammadsaleh <mehemmedsalehabbas@gmail.com>
2024-01-20 01:36:32 -0800

    binary search algorith was added

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,4 +1,6 @@
 #ifndef SEARCH
 #define SEARCH
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void binary_printer(int *arr, size_t mid, size_t size);
 #endif
